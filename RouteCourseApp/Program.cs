d581d3d3c70846b5a7ff6111daac7003b06617c7
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-08-21 08:16:40 +0600

    Задача Парное программирование решена

--- RouteCourseApp/Program.cs
+++ RouteCourseApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace RouteCourseApp
@@ -8,15 +10,44 @@ namespace RouteCourseApp
         static void Main(string[] args)
         {
             var setsNumb = int.Parse(Console.ReadLine());
+
             for (int i = 0; i < setsNumb; i++)
             {
-                var arrNumb = int.Parse(Console.ReadLine());
-                string source = Console.ReadLine();
-                var productSum = (from price in source.Split(' ')
-                                  group price by price into g
-                                  select int.Parse(g.Key) * (g.Count() - (g.Count() / 3))).Sum();
-                Console.WriteLine(productSum);
+                var countDevelopers = int.Parse(Console.ReadLine());
+                var arrLevel = Console.ReadLine().Split().Select((x, i) => new Level { Index = i, Value = int.Parse(x) });
+                for (int j = 0; j < countDevelopers / 2; j++)
+                {
+                    var current = arrLevel.First();
+                    var currentArr = arrLevel.Where(x => x.Index != current.Index).ToList();
+
+                    var min = currentArr.Select(x => (int)Math.Abs(current.Value - x.Value)).OrderBy(x => x).First();
+                    var next = currentArr.First(x => x.Value == (current.Value + min) || x.Value == (current.Value - min));
+
+                    Console.WriteLine($"{current.Index + 1} {next.Index + 1}");
+                    arrLevel = arrLevel.Where(x => x.Index != current.Index && x.Index != next.Index).ToList();
+                }
+                Console.WriteLine();
             }
+        }
+    }
+
+    class Level
+    {
+        public int Index { get; set; }
+        public int Value { get; set; }
+    }
+
+    class LevelComparer : EqualityComparer<Level>
+    {
+        public override bool Equals(Level x, Level y)
+        {
+            return x.Value.Equals(y.Value);
+        }
+
+        public override int GetHashCode([DisallowNull] Level obj)
+        {
+            string value = obj.Value.ToString();
+            return value.GetHashCode();
         }
     }
 }
