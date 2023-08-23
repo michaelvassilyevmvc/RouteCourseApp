eb1707f2c4a131a80281daf6916b92d23e3b78b0
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-08-24 01:00:08 +0600

    Task E completed

--- RouteCourseApp/Program.cs
+++ RouteCourseApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -8,104 +9,45 @@ namespace RouteCourseApp
     {
         static void Main(string[] args)
         {
+            
             var setsNum = int.Parse(Console.ReadLine());
-            Console.WriteLine();
 
             for (int r = 0; r < setsNum; r++)
             {
-                var rowAndCol = Console.ReadLine().Split();
-                int row = int.Parse(rowAndCol[0]);
-                int col = int.Parse(rowAndCol[1]);
-                int[,] arr = new int[row, col];
+                var itemCount = int.Parse(Console.ReadLine());
+                var sourceArr = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+                Dictionary<int, int> dict = new Dictionary<int, int>();
+                bool hasMistake = false;
 
-                for (int i = 0; i < row; i++)
+                for (int i = 0; i < itemCount; i++)
                 {
-                    var rowStr = Console.ReadLine().Split();
-                    for (int j = 0; j < rowStr.Length; j++)
+                    if (!dict.ContainsKey(sourceArr[i]) )
                     {
-                        arr[i, j] = int.Parse(rowStr[j]);
+                        dict.Add(sourceArr[i], i);
                     }
+                    else if (i - dict[sourceArr[i]] == 1)
+                    {
+                        dict[sourceArr[i]] = i;
+                    }
+                    else
+                    {
+                        hasMistake = true;
+                        break;
+                    }
+
                 }
-
-                int clicksCount = int.Parse(Console.ReadLine());
-                var clickOrderArr = Console.ReadLine().NoRepeat().Split().Select(x => int.Parse(x) - 1).ToArray();
-
-
-
-                Console.WriteLine();
-                for (int s = 0; s < clickOrderArr.Count(); s++)
-                {
-                    arr = SortTable(arr, clickOrderArr[s]);
-                }
-
-                ShowTable(arr);
-                Console.WriteLine();
+                
+                Console.WriteLine(GetAnswer(hasMistake));
+                
             }
         }
 
+        public static string GetAnswer(bool flag) => flag ? "NO" : "YES";
 
-        private static int[,] SortTable(int[,] arr, int col)
-        {
-            int[,] resArr = new int[arr.GetLength(0), arr.GetLength(1)];
-
-            var needSortCol = CustomArray<int>.GetColumn(arr, col).Select((x, i) => new { Index = i, Value = x }).OrderBy(x => x.Value).ToArray();
-
-            for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    resArr[i, j] = arr[needSortCol[i].Index, j];
-                }
-            }
-            return resArr;
-        }
-
-        private static void ShowTable(int[,] arr)
-        {
-            for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    Console.Write(arr[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
-        }
+        
     }
 
-    public static class MyExtensions
-    {
-        public static string NoRepeat(this string value)
-        {
-            StringBuilder res = new StringBuilder();
-            foreach (char c in value)
-            {
-                if (res.Length == 0 || !res[res.Length - 1].Equals(c))
-                {
-                    res.Append(c);
-                }
-            }
-            return res.ToString();
-        }
-    }
-
-    public static class CustomArray<T>
-    {
-        public static T[] GetColumn(T[,] matrix, int columnNumber)
-        {
-            return Enumerable.Range(0, matrix.GetLength(0))
-                    .Select(x => matrix[x, columnNumber])
-                    .ToArray();
-        }
-
-        public static T[] GetRow(T[,] matrix, int rowNumber)
-        {
-            return Enumerable.Range(0, matrix.GetLength(1))
-                    .Select(x => matrix[rowNumber, x])
-                    .ToArray();
-        }
-
-    }
+   
 
 
 }
