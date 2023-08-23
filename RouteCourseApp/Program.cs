beafdc20b8f2d503923988c85d03e31b6944dd2f
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2023-08-23 18:41:09 +0600

    TaskD. Not Accesable Solution

--- RouteCourseApp/Program.cs
+++ RouteCourseApp/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Text;
 
 namespace RouteCourseApp
 {
@@ -9,45 +8,104 @@ namespace RouteCourseApp
     {
         static void Main(string[] args)
         {
-            var setsNumb = int.Parse(Console.ReadLine());
+            var setsNum = int.Parse(Console.ReadLine());
+            Console.WriteLine();
 
-            for (int i = 0; i < setsNumb; i++)
+            for (int r = 0; r < setsNum; r++)
             {
-                var countDevelopers = int.Parse(Console.ReadLine());
-                var arrLevel = Console.ReadLine().Split().Select((x, i) => new Level { Index = i, Value = int.Parse(x) });
-                for (int j = 0; j < countDevelopers / 2; j++)
+                var rowAndCol = Console.ReadLine().Split();
+                int row = int.Parse(rowAndCol[0]);
+                int col = int.Parse(rowAndCol[1]);
+                int[,] arr = new int[row, col];
+
+                for (int i = 0; i < row; i++)
                 {
-                    var current = arrLevel.First();
-                    var currentArr = arrLevel.Where(x => x.Index != current.Index).ToList();
+                    var rowStr = Console.ReadLine().Split();
+                    for (int j = 0; j < rowStr.Length; j++)
+                    {
+                        arr[i, j] = int.Parse(rowStr[j]);
+                    }
+                }
 
-                    var min = currentArr.Select(x => (int)Math.Abs(current.Value - x.Value)).OrderBy(x => x).First();
-                    var next = currentArr.First(x => x.Value == (current.Value + min) || x.Value == (current.Value - min));
+                int clicksCount = int.Parse(Console.ReadLine());
+                var clickOrderArr = Console.ReadLine().NoRepeat().Split().Select(x => int.Parse(x) - 1).ToArray();
 
-                    Console.WriteLine($"{current.Index + 1} {next.Index + 1}");
-                    arrLevel = arrLevel.Where(x => x.Index != current.Index && x.Index != next.Index).ToList();
+
+
+                Console.WriteLine();
+                for (int s = 0; s < clickOrderArr.Count(); s++)
+                {
+                    arr = SortTable(arr, clickOrderArr[s]);
+                }
+
+                ShowTable(arr);
+                Console.WriteLine();
+            }
+        }
+
+
+        private static int[,] SortTable(int[,] arr, int col)
+        {
+            int[,] resArr = new int[arr.GetLength(0), arr.GetLength(1)];
+
+            var needSortCol = CustomArray<int>.GetColumn(arr, col).Select((x, i) => new { Index = i, Value = x }).OrderBy(x => x.Value).ToArray();
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    resArr[i, j] = arr[needSortCol[i].Index, j];
+                }
+            }
+            return resArr;
+        }
+
+        private static void ShowTable(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
                 }
                 Console.WriteLine();
             }
         }
     }
 
-    class Level
+    public static class MyExtensions
     {
-        public int Index { get; set; }
-        public int Value { get; set; }
-    }
-
-    class LevelComparer : EqualityComparer<Level>
-    {
-        public override bool Equals(Level x, Level y)
+        public static string NoRepeat(this string value)
         {
-            return x.Value.Equals(y.Value);
-        }
-
-        public override int GetHashCode([DisallowNull] Level obj)
-        {
-            string value = obj.Value.ToString();
-            return value.GetHashCode();
+            StringBuilder res = new StringBuilder();
+            foreach (char c in value)
+            {
+                if (res.Length == 0 || !res[res.Length - 1].Equals(c))
+                {
+                    res.Append(c);
+                }
+            }
+            return res.ToString();
         }
     }
+
+    public static class CustomArray<T>
+    {
+        public static T[] GetColumn(T[,] matrix, int columnNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(0))
+                    .Select(x => matrix[x, columnNumber])
+                    .ToArray();
+        }
+
+        public static T[] GetRow(T[,] matrix, int rowNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(1))
+                    .Select(x => matrix[rowNumber, x])
+                    .ToArray();
+        }
+
+    }
+
+
 }
